70b4e12b77bad9af29a044e4bd8474315dd18884
Mohit Singh <qmohitsingh@gmail.com>
2023-10-26 11:56:50 -0400

    added error.jsx file and routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GameMenuController from "./Components/GameMenuController";
 import {Typography} from "@mui/material";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./Components/Error";
 
 const router = createBrowserRouter([
     {
@@ -27,25 +28,7 @@ const router = createBrowserRouter([
                 </Typography>
                 <GameMenuController />
             </div>,
-        errorElement:
-            <div className="mt-5">
-                <Typography
-                    variant="h4"
-                    align="center"
-                    gutterBottom
-                    style={{
-                        color: '#fff',
-                        textShadow: '2px 2px 4px rgba(255, 255, 255, 0.7)', // Add text shadow for a shiny effect
-                        fontFamily: 'cursive', // Use a cursive font for a fancy look
-                        fontSize: '2.5rem', // Increase font size
-                        letterSpacing: '2px', // Add letter spacing for a fancy touch
-                    }}
-                    className="w-full"
-                >
-                    Emojination: Memory Adventures
-                </Typography>
-                <GameMenuController />
-            </div>,
+        errorElement: <Error />,
     }]);
 
 function App() {
