54e07ef2163c1c7c2e75a34e14fc64cb09d1a3ad
Suraj Khamkar <surajk@atlanticpkg.com>
2025-01-18 19:39:58 -0500

    Fixed audio issue

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import GameMenuController from "./Components/GameMenuController";
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Error from "./Components/Error";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element:
-            <div className="mt-5">
-                <Typography
-                    variant="h4"
-                    align="center"
-                    gutterBottom
-                    style={{
-                        color: '#fff',
-                        textShadow: '2px 2px 4px rgba(255, 255, 255, 0.7)', // Add text shadow for a shiny effect
-                        fontFamily: 'cursive', // Use a cursive font for a fancy look
-                        fontSize: '2.5rem', // Increase font size
-                        letterSpacing: '2px', // Add letter spacing for a fancy touch
-                    }}
-                    className="w-full"
-                >
-                    Emojination: Memory Adventures
-                </Typography>
-                <GameMenuController />
-            </div>,
-        errorElement: <Error />,
-    }]);
+  {
+    path: "/",
+    element: (
+      <div className="mt-5">
+        <Typography
+          variant="h4"
+          align="center"
+          gutterBottom
+          style={{
+            color: "#fff",
+            textShadow: "2px 2px 4px rgba(255, 255, 255, 0.7)", // Add text shadow for a shiny effect
+            fontFamily: "cursive", // Use a cursive font for a fancy look
+            fontSize: "2.5rem", // Increase font size
+            letterSpacing: "2px", // Add letter spacing for a fancy touch
+          }}
+          className="w-full"
+        >
+          Emojination: Memory Adventures
+        </Typography>
+        <GameMenuController />
+      </div>
+    ),
+    errorElement: <Error />,
+  },
+]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Components/GameMenuController.jsx
+++ src/Components/GameMenuController.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 import LevelSelection from "./LevelSelection";
 import HighScore from "./HighScore";
 import About from "./About";
@@ -9,39 +9,88 @@ import HardGameMode from "./HardGameMode";
 import Setting from "./Setting";
 
 const GameMenuController = () => {
-    const [gameState, setGameState] = useState(0);
-    const audioOn = localStorage.getItem('audioOn') === '1' ? true : false;
+  const [gameState, setGameState] = useState(0);
+  const audioOn = localStorage.getItem("audioOn") === "1" ? true : false;
 
-    const scrollRef = useRef(null);
+  const scrollRef = useRef(null);
 
-    const onGameStateChange = (gameState) => {
-        setGameState(gameState);
-    };
+  const onGameStateChange = (gameState) => {
+    setGameState(gameState);
+  };
 
-    useEffect(() => {
-        if (gameState === 5 && scrollRef.current) {
-            scrollRef.current.scrollIntoView({
-                behavior: 'smooth',
-                block: 'nearest',
-                inline: 'center'});
-        }
-    }, [gameState]);
+  // Set `audioOn` in localStorage on page load
+  useEffect(() => {
+    localStorage.setItem("audioOn", "1");
+  }, []);
 
-    return (
-        <div>
-            <div className="mt-10">
-                {gameState === 0 ? <GameMenu onGameStateChange={onGameStateChange} /> : ''}
-                {gameState === 1 ? <LevelSelection onGameStateChange={onGameStateChange} /> : ''}
-                {gameState === 2 ? <HighScore onGameStateChange={onGameStateChange} /> : ''}
-                {gameState === 3 ? <Setting onGameStateChange={onGameStateChange} /> : ''}
-                {gameState === 4 ? <About onGameStateChange={onGameStateChange} /> : ''}
-                {gameState === 5 ? <EasyGameMode onGameStateChange={onGameStateChange} soundEnabled={audioOn} /> : ''}
-                {gameState === 6 ? <MediumGameMode onGameStateChange={onGameStateChange} soundEnabled={audioOn} /> : ''}
-                {gameState === 7 ? <HardGameMode onGameStateChange={onGameStateChange} soundEnabled={audioOn} /> : ''}
-            </div>
-            {gameState === 5 ? <div className="m-20" ref={scrollRef}><hr /></div> : ""}
+  useEffect(() => {
+    if (gameState === 5 && scrollRef.current) {
+      scrollRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "center",
+      });
+    }
+  }, [gameState]);
+
+  return (
+    <div>
+      <div className="mt-10">
+        {gameState === 0 ? (
+          <GameMenu onGameStateChange={onGameStateChange} />
+        ) : (
+          ""
+        )}
+        {gameState === 1 ? (
+          <LevelSelection onGameStateChange={onGameStateChange} />
+        ) : (
+          ""
+        )}
+        {gameState === 2 ? (
+          <HighScore onGameStateChange={onGameStateChange} />
+        ) : (
+          ""
+        )}
+        {gameState === 3 ? (
+          <Setting onGameStateChange={onGameStateChange} />
+        ) : (
+          ""
+        )}
+        {gameState === 4 ? <About onGameStateChange={onGameStateChange} /> : ""}
+        {gameState === 5 ? (
+          <EasyGameMode
+            onGameStateChange={onGameStateChange}
+            soundEnabled={audioOn}
+          />
+        ) : (
+          ""
+        )}
+        {gameState === 6 ? (
+          <MediumGameMode
+            onGameStateChange={onGameStateChange}
+            soundEnabled={audioOn}
+          />
+        ) : (
+          ""
+        )}
+        {gameState === 7 ? (
+          <HardGameMode
+            onGameStateChange={onGameStateChange}
+            soundEnabled={audioOn}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+      {gameState === 5 ? (
+        <div className="m-20" ref={scrollRef}>
+          <hr />
         </div>
-    );
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default GameMenuController;
